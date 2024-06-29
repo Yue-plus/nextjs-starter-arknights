d2b96b5f360a0821ed62ce5d7e80e20fea591978
Yue-plus <Yue_plus@foxmail.com>
2024-06-29 12:01:54 +0800

    feat(ToolBox): Social Dialog Icons

--- app/_components/ToolBox.tsx
+++ app/_components/ToolBox.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {
     IconBiliBili,
-    IconSkland,
+    IconSkland, IconSocial,
     IconTapTap,
     IconWechat,
     IconWeibo,
@@ -37,13 +37,21 @@ export default function ToolBox({state: [opened, setOpened]}: ToolBoxProps) {
                          absolute top-[6.75rem] portrait:top-1/2 right-0 portrait:left-1/2
                          transition duration-300
                          portrait:-translate-x-1/2 portrait:-translate-y-1/2`}>
-            <div className={`border-[1rem] border-solid border-transparent border-b-ark-blue
-                             absolute bottom-full left-[2rem]
-                             portrait:hidden`}></div>
             <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
                 <LogoRhodesIsland className={`h-[59.375rem] portrait:h-[55rem] text-[#06bbff]
                                               absolute top-[-2rem] left-[-18.75rem] portrait:left-[-5rem]`}/>
             </div>
+            <div className={`w-[4.5rem] h-[4.5rem] mt-[.5rem] text-white
+                             absolute right-0 top-0 transition duration-300 cursor-pointer
+                             hidden portrait:block`}>
+                <div className="w-[.25rem] h-4/5 bg-white absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rotate-45"></div>
+                <div className="w-[.25rem] h-4/5 bg-white absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -rotate-45"></div>
+            </div>
+            <IconSocial
+                className="w-[3rem] h-auto absolute left-[3.5rem] top-[7.5rem] pointer-events-none hidden portrait:block"/>
+            <div className={`border-[1rem] border-solid border-transparent border-b-ark-blue
+                             absolute bottom-full left-[2rem]
+                             portrait:hidden`}></div>
             <div className="portrait:w-[25.75rem] relative pt-[2.625rem] portrait:pt-0 pb-[3rem] portrait:m-auto">
                 <div className={`mx-12 portrait:mr-0 portrait:ml-0
                                  whitespace-nowrap text-[.75rem] portrait:text-[1.125rem]
@@ -57,13 +65,15 @@ export default function ToolBox({state: [opened, setOpened]}: ToolBoxProps) {
                                        text-white bg-black transition duration-300
                                        portrait:border portrait:border-solid portrait:border-white
                                        flex flex-none rounded-lg portrait:rounded-2xl cursor-pointer`}>
-                            <IconSkland className={`w-[2rem] portrait:w-[4rem] h-auto m-auto pointer-events-none block`}/>
+                            <IconSkland
+                                className={`w-[2rem] portrait:w-[4rem] h-auto m-auto pointer-events-none block`}/>
                         </a>
                         <a className={`w-[3.375rem] portrait:w-[6.375rem] h-[3.375rem] portrait:h-[6.375rem]
                                        text-white bg-black transition duration-300
                                        portrait:border portrait:border-solid portrait:border-white
                                        flex flex-none rounded-lg portrait:rounded-2xl cursor-pointer`}>
-                            <IconBiliBili className={`w-[2.75rem] portrait:w-[5rem] h-auto m-auto pointer-events-none block`}/>
+                            <IconBiliBili
+                                className={`w-[2.75rem] portrait:w-[5rem] h-auto m-auto pointer-events-none block`}/>
                         </a>
                     </div>
                     {/* TODO: 调整其他图标 className */}
