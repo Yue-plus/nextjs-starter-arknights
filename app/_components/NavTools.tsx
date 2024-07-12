4d996d4492d18a484748ab86ab52264f2022ce8f
Yue-plus <Yue_plus@foxmail.com>
2024-07-13 00:05:28 +0800

    feat(Sound): 播放 BGM

--- app/_components/NavTools.tsx
+++ app/_components/NavTools.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {IconSocial, IconSound, IconUser} from "@/app/_components/SvgIcons";
 
 const ActiveColor = "#18d1ff"
@@ -20,12 +20,23 @@ export function Social({state: [active, setActive]}: NavToolProps) {
 
 export function Sound() {
     const [active, setActive] = useState(false)
+    const audioRef = useRef<HTMLAudioElement>(null)
+
+    useEffect(() => {
+        if (audioRef.current) {
+            if (active) audioRef.current.play().catch(e => console.error(e));
+            else audioRef.current.pause()
+        }
+    }, [active])
+
     return <div className={BoxClassName} onClick={_ => setActive(!active)}>
         <IconSound className={SvgClassName} style={{
             color: active ? ActiveColor : InactiveColor,
             transition: "transform .3s",
             transform: `scaleY(${active ? 1 : .5})`,
         }}/>
+        {/* https://web.hycdn.cn/arknights/official/_next/static/media/audio/bgm.ea4286.mp3 */}
+        <audio ref={audioRef} src="/media/audio/bgm.mp3" autoPlay/>
     </div>
 }
 
