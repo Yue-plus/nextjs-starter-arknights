efc552cc0ab33fd9274e2ffac250ebc38d776d4f
Yue-plus <Yue_plus@foxmail.com>
2024-07-16 01:39:38 +0800

    perf(ResponsiveFontSize)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type {Metadata} from "next";
 import "./globals.css";
 import {inter} from "@/app/_fonts/fonts";
 import {SvgDefs} from "@/app/_components/SvgIcons";
+import ResponsiveFontSize from "@/app/_components/ResponsiveFontSize";
 
 export const metadata: Metadata = {
     title: "Arknights",
@@ -13,6 +14,7 @@ export default function RootLayout({children}: Readonly<{
     children: React.ReactNode;
 }>) {
     return <html lang="zh" className="h-full" style={{fontSize: "16px"}}>
+        <ResponsiveFontSize />
         <body className={`${inter.className} w-full h-full text-white bg-black m-0 overflow-hidden`}>
             <div className="w-full h-full relative bg-[url('/media/bg.jpg')] bg-center bg-cover bg-no-repeat">
                 {children}
